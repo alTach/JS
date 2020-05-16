1d498ff13b96ff031e774e8dfb6158434960cb0b
Алиев Турач Сураддин Оглы <taliev@stream.ru>
2020-05-16 22:55:13 +0300

    fix

--- svelte/svelte-step-2/src/App.svelte
+++ svelte/svelte-step-2/src/App.svelte
@@ -1,6 +1,40 @@
 <script>
-    import Header from './UI/Header.svelte'
+    import Product from "./Product.svelte";
+    import Modal from "./Modal.svelte";
+
+    let showModal = false;
+    let products = [
+        {
+            id: "p1",
+            title: "A boo",
+            price: 9.99
+        }
+    ];
+
+    function addToCart(event) {
+        console.log(event);
+    }
+
+    function deleteProduct(event) {
+        console.log(event.detail);
+    }
 </script>
 
+{#each products as product}
+    <Product
+            {...product}
+            on:add-to-cart={addToCart}
+            on:delete={deleteProduct}/>
+{/each}
 
-<Header/>
+
+<button on:click={() => showModal = true}>Show modal</button>
+
+{#if showModal}
+  <Modal>
+      <header slot="header">
+          header
+      </header>
+      <div class="body" slot="body">body</div>
+  </Modal>
+{/if}
---
+++ svelte/svelte-step-2/src/Modal.svelte
@@ -0,0 +1,9 @@
+<script>
+    import {onMount, onDestroy} from 'svelte'
+
+    onMount(() => console.log('onMount'));
+    onDestroy(() => console.log('onDestroy'));
+</script>
+
+<div class="backdrop"></div>
+<slot name="header"></slot>
